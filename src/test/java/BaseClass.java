a675015edbb010c0e9cfcfdc98c9363a7d6175c5
gopiklr5 <gopiklr5@gmail.com>
2025-07-20 22:07:48 +0530

    updated

--- src/test/java/BaseClass.java
+++ src/test/java/BaseClass.java
@@ -1,12 +1,42 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class BaseClass {
+import java.net.MalformedURLException;
 
+public class BaseClass {
+    private WebDriver driver;
+
+
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        ChromeOptions options = new ChromeOptions();
+
+    }
 
     @Test
-    public void test01(){
+    public void testGoogleSearch() {
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com");
 
+    /*    WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Selenium GitHub Actions");
+        searchBox.submit();
 
-        System.out.print("Testing...........");
+        // Basic check: Page title contains search term
+        assert driver.getTitle().toLowerCase().contains("selenium github actions");*/
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
